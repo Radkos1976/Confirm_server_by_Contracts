d282ccb4522b1163178dbc3a1e28a96399259115
Radomir Kosobucki <radkos76@gmail.com>
2024-01-03 13:08:20 +0100

    fix connection

--- Confirm_server_by_Contracts/Lack_report.cs
+++ Confirm_server_by_Contracts/Lack_report.cs
@@ -51,7 +51,7 @@ namespace Confirm_server_by_Contracts
                 );
         }
         private async Task<List<Lack_report_row>> Old_data(CancellationToken cancellationToken) => await rw.Get_PSTGR("SELECT * FROM day_qty", "Lack_report", cancellationToken);
-        private async Task<List<Lack_report_row>> New_data(CancellationToken cancellationToken) => await rw.Get_PSTGR("" +
+        private async Task<List<Lack_report_row>> New_data(CancellationToken cancellationToken) => await rw.Get_Ora("" +
                 @"SELECT                     
                     a.* 
                 FROM 
